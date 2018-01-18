fd7d33e36e1615eadabbcb27dd8cbba776fec9fd
biolab <biolab@biolabs-air.fri1.uni-lj.si>
2018-01-18 11:50:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+Orange3-bioinformatics
+===================
+
+Orange Bioinformatics extends Orange_, a data mining software
+package, with common functionality for bioinformatics. The provided
+functionality can be accessed as a Python library or through a visual
+programming interface (Orange Canvas). The latter is also suitable for
+non-programmers.
+
+In Orange Canvas the analyst connects basic computational units, called
+widgets, into data flow analytics schemas. Two units-widgets can be
+connected if they share a data type. Compared to other popular tools like
+Taverna, Orange widgets are high-level, integrated potentially complex
+tasks, but are specific enough to be used independently. Even elaborate
+analyses rarely consist of more than ten widgets; while tasks such as
+clustering and enrichment analysis could be executed with up to five
+widgets. While building the schema each widget is independently controlled
+with settings, the settings do not conceptually burden the analyst.
+
+Orange Bioinformatics provides access to publicly available data, like GEO data sets, GO and KEGG.
+All features can be combined with powerful visualization, network exploration and
+data mining techniques from the Orange data mining framework.
+
+Installation
+------------
+
+To install the add-on with pip use
+
+    pip install Orange3-bioinformatics
+
+To register this add-on with Orange, but keep the code in the development directory (do not copy it to
+Python's site-packages directory), run
+
+    pip install -e .
+
+Documentation / widget help can be built by running
+
+    make html htmlhelp
+
+from the doc directory.
+
+Usage
+-----
+
+After the installation, the widgets from this add-on are registered with Orange. To run Orange from the terminal, use
+
+    python3 -m Orange.canvas
+    
+or
+
+    orange-canvas
+
+The new widgets are in the toolbox bar under Bioinformatics section.
---
+++ setup.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+import os
+
+
+from setuptools import setup, find_packages
+
+
+NAME = 'Orange-bioinformatics'
+DOCUMENTATION_NAME = 'Orange Bioinformatics'
+VERSION = '3.0.0'
+
+DESCRIPTION = 'Orange Bioinformatics add-on for Orange data mining software package.'
+LONG_DESCRIPTION = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+AUTHOR = 'Bioinformatics Laboratory, FRI UL'
+AUTHOR_EMAIL = 'info@biolab.si'
+URL = 'https://github.com/biolab/orange3-bioinformatics'
+LICENSE = 'GPL3+'
+
+KEYWORDS = (
+    # [PyPi](https://pypi.python.org) packages with keyword "orange3 add-on"
+    # can be installed using the Orange Add-on Manager
+    # 'orange3 add-on'
+)
+
+CLASSIFIERS = (
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Scientific/Engineering :: Visualization',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Intended Audience :: Education',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Developers',
+    'Development Status :: 1 - Planning',
+    'Programming Language :: Python :: 3 :: Only',
+    'Operating System :: OS Independent'
+)
+
+PACKAGES = find_packages()
+PACKAGE_DATA = {}
+
+requirements = ['requirements.txt']
+
+INSTALL_REQUIRES = sorted(set(
+    line.partition('#')[0].strip()
+    for file in (os.path.join(os.path.dirname(__file__), file)
+                 for file in requirements)
+    for line in open(file)
+) - {''})
+
+
+ENTRY_POINTS = {
+    'orange3.addon': (
+        'bioinformatics = orangecontrib.bioinformatics'
+    ),
+    'orange.widgets': (
+        'Bioinformatics = orangecontrib.bioinformatics.widgets'
+    ),
+    'orange.canvas.help': (
+        'html-index = orangecontrib.bioinformatics.widgets:WIDGET_HELP_PATH'
+    )
+}
+
+NAMESPACE_PACAKGES = ["orangecontrib"]
+
+if __name__ == '__main__':
+    setup(
+        name=NAME,
+        version=VERSION,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        url=URL,
+        classifiers=CLASSIFIERS,
+        license=LICENSE,
+        keywords=KEYWORDS,
+        packages=PACKAGES,
+        package_data=PACKAGE_DATA,
+        install_requires=INSTALL_REQUIRES,
+        entry_points=ENTRY_POINTS,
+        namespace_packages=NAMESPACE_PACAKGES,
+        include_package_data=True,
+        zip_safe=False,
+    )
