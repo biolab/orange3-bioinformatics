01d1f1104836adbfd5f3e3c786b3fe3ca0e29b90
JakaKokosar <jaka.kokosar@gmail.com>
2019-02-10 14:22:25 +0100

    widgets: add missing __init__ file

---
+++ orangecontrib/bioinformatics/widgets/__init__.py
@@ -0,0 +1,20 @@
+""" Bioinformatics """
+
+ICON = "icons/Category-Bioinformatics.svg"
+
+BACKGROUND = "light-grass"
+
+# Location of widget help files.
+WIDGET_HELP_PATH = (
+    # Used for development.
+    # You still need to build help pages using
+    # make htmlhelp
+    # inside doc folder
+    ("{DEVELOP_ROOT}/doc/_build/htmlhelp/index.html", None),
+
+    # Online documentation url, used when the local documentation is available.
+    # Url should point to a page with a section Widgets. This section should
+    # includes links to documentation pages of each widget. Matching is
+    # performed by comparing link caption to widget name.
+    ("https://orange3-bioinformatics.readthedocs.io/en/latest/", "")
+)
