9183981ac33baa462fe660cad1a1a8b507f8c132
Primož Godec <p.godec9@gmail.com>
2019-05-16 17:21:12 +0200

    Tests for annotate_samples

---
+++ orangecontrib/bioinformatics/tests/annotation/test_annotate_samples.py
@@ -0,0 +1,43 @@
+import unittest
+
+import numpy as np
+from Orange.data import Table, Domain, StringVariable, ContinuousVariable
+
+from orangecontrib.bioinformatics.annotation.annotate_samples import \
+    AnnotateSamples
+
+
+class TestAnnotateSamples(unittest.TestCase):
+
+    def setUp(self):
+        m_domain = Domain(
+            [], None, [
+                StringVariable("Cell Type"), StringVariable("Entrez ID")])
+        m_data = [["Type 1", 111], ["Type 1", 112], ["Type 1", 113],
+                   ["Type 2", 211], ["Type 2", 212], ["Type 2", 213]]
+        self.markers = Table(m_domain, np.empty((len(m_data), 0)), None, m_data)
+
+        genes = [111, 112, 113, 211, 212, 213]
+        domain = Domain([ContinuousVariable(
+            str(g)) for g in genes])
+        for v, g in zip(domain.attributes, genes):
+            v.attributes ={"Entrez ID": g}
+        self.data = Table(domain, np.array(
+            [[1, 1, 1, 0, 0, 0],
+             [1, .8, .9, 0, 0, 0],
+             [.7, 1.1, 1, 0, 0, 0],
+             [.8, .7, 1.1, 0, .1, 0],
+             [0, 0, 0, 1.05, 1.05, 1.1],
+             [0, 0, 0, 1.1, 1.0, 1.05],
+             [0, 0, 0, 1.05, .9, 1.1],
+             [0, 0, 0, .9, .9, 1.2]
+             ]))
+
+    def test_artificial_data(self):
+        annotator = AnnotateSamples(p_value_th=0.05)
+        annotations = annotator.annotate_samples(self.data, self.markers)
+
+        self.assertEqual(type(annotations), Table)
+        self.assertEqual(len(annotations), len(self.data))
+        self.assertEqual(len(annotations[0]), 2)  # two types in the data
+        self.assertGreater(annotations.X.sum(), 0)
