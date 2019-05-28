f9024cb95ec50c951424e7c311cae9520faec2f1
Primož Godec <p.godec9@gmail.com>
2019-05-28 10:23:47 +0200

    Annotate Samples: Small implementation changes

--- orangecontrib/bioinformatics/annotation/annotate_samples.py
+++ orangecontrib/bioinformatics/annotation/annotate_samples.py
@@ -44,21 +44,26 @@ class AnnotateSamples:
     p_value_th : float
         A threshold for accepting the annotations. Annotations that has FDR
         value bellow this threshold are used.
-    p_value_fun : callable, optional (defaults: statistics.Binomial().p_value)
+    p_value_fun : str, optional (defaults: TEST_BINOMIAL)
         A function that calculates p-value. It can be either
-        statistics.Binomial().p_value or hypergeom.sf.
+        TEST_BINOMIAL that uses statistics.Binomial().p_value or
+        TEST_HYPERGEOMETRIC that uses hypergeom.sf.
+    z_threshold : float
+        The threshold for selecting the attribute. For each item the attributes
+        with z-value above this value are selected.
     """
 
-    def __init__(self, p_value_th, p_value_fun=statistics.Binomial().p_value):
-        if p_value_fun == hypergeom.sf:  # because sf accept x-1 instead of x
-            self.p_value_fun = lambda x, n, m, k: p_value_fun(x-1, n, m, k)
+    def __init__(self, p_value_th, p_value_fun="TEST_BINOMIAL", z_threshold=1):
+        if p_value_fun == "TEST_HYPERGEOMETRIC":  # sf accept x-1 instead of x
+            self.p_value_fun = lambda x, n, m, k: hypergeom.sf(x-1, n, m, k)
         else:
-            self.p_value_fun = p_value_fun
+            self.p_value_fun = statistics.Binomial().p_value
 
         self.p_threshold = p_value_th
+        self.z_threshold = z_threshold
 
     @staticmethod
-    def _select_genes(data):
+    def select_attributes(data, z_threshold=1):
         """
         Function selects "over"-expressed attributes for items with Mann-Whitney
         U test.
@@ -67,6 +72,9 @@ class AnnotateSamples:
         ----------
         data : Orange.data.Table
             Tabular data
+        z_threshold : float
+            The threshold for selecting the attribute. For each item the
+            attributes with z-value above this value are selected.
 
         Returns
         -------
@@ -96,8 +104,15 @@ class AnnotateSamples:
         # gene selection
         attributes_np = np.array([
             a.attributes.get("Entrez ID") for a in data.domain.attributes])
-        attributes_sets = [set(attributes_np[row > 1]) - {None} for row in z]
-        return attributes_sets, z
+        attributes_sets = [
+            set(attributes_np[row > z_threshold]) - {None} for row in z]
+
+        # pack z values to data table
+        # take only attributes in new domain
+        domain = Domain([x for x in data.domain.attributes])
+        z_table = Table(domain, z)
+
+        return attributes_sets, z_table
 
     @staticmethod
     def _group_marker_attributes(markers):
@@ -112,7 +127,7 @@ class AnnotateSamples:
                 types_dict[str(m["Cell Type"])].add(int(m["Entrez ID"].value))
         return types_dict
 
-    def _assign_annotations(self, items_sets, available_annotations, tax_id):
+    def assign_annotations(self, items_sets, available_annotations, tax_id):
         """
         Function get set of attributes (e.g. genes) that represents each item
         and attributes for each annotation. It returns the annotations most
@@ -124,13 +139,15 @@ class AnnotateSamples:
             Set of most important attributes for each item.
         available_annotations : Orange.data.Table
             Available annotations (e.g. cell types)
+        tax_id : str
+            The id of the organism
 
         Returns
         -------
-        ndarray
+        Orange.data.Table
             Annotation probabilities
-        list
-            Annotation list
+        Orange.data.Table
+            Annotation fdrs
         """
         # retrieve number of genes for organism
         N = len(GeneInfo(tax_id))
@@ -141,7 +158,7 @@ class AnnotateSamples:
 
         def hg_cell(item_attributes):
             p_values = []
-            prob = np.zeros(len(grouped_annotations_items))
+            prob = []
             for i, (ct, attributes) in enumerate(grouped_annotations_items):
                 x = len(item_attributes & attributes)
                 k = len(item_attributes)  # drawn balls - expressed for item
@@ -150,14 +167,21 @@ class AnnotateSamples:
                 p_value = self.p_value_fun(x, N, m, k)
 
                 p_values.append(p_value)
-                prob[i] = x / (m + 1e-16)
+                prob.append(x / (m + 1e-16))
 
             fdrs = statistics.FDR(p_values)
-            prob[np.array(fdrs) > self.p_threshold] = 0
-            return prob
+            # prob[np.array(fdrs) > self.p_threshold] = 0
+            return prob, fdrs
 
-        return np.array([hg_cell(cg) for cg in items_sets]), \
-            [x[0] for x in grouped_annotations_items]
+        prob_fdrs = map(hg_cell, items_sets)
+        probs, fdrs = zip(*prob_fdrs)
+
+        domain = Domain(
+            [ContinuousVariable(ct[0]) for ct in grouped_annotations_items])
+        probs_table = Table(domain, np.array(probs))
+        fdrs_table = Table(domain, np.array(fdrs))
+
+        return probs_table, fdrs_table
 
     def annotate_samples(self, data, available_annotations,
                          return_nonzero_annotations=True):
@@ -170,7 +194,7 @@ class AnnotateSamples:
             Tabular data
         available_annotations : Orange.data.Table
             Available annotations (e.g. cell types)
-        return_nonzero_annotations : bool
+        return_nonzero_annotations : bool, optional (default=True)
             If true return scores for only annotations present in at least one
             sample.
 
@@ -186,17 +210,16 @@ class AnnotateSamples:
 
         tax_id = data.attributes[TAX_ID]
 
-        selected_attributes, z = self._select_genes(data)
-        annotations_scores, annotations = self._assign_annotations(
+        selected_attributes, z = self.select_attributes(
+            data, z_threshold=self.z_threshold)
+        annotation_probs, annotation_fdrs = self.assign_annotations(
             selected_attributes, available_annotations, tax_id)
+        annotation_probs.X[annotation_fdrs.X > self.p_threshold] = 0
 
         if return_nonzero_annotations:
-            col_nonzero = np.sum(annotations_scores, axis=0) > 0
-            annotations_scores = annotations_scores[:, col_nonzero]
-            annotations = np.array(annotations)[col_nonzero]
+            col_nonzero = np.sum(annotation_probs.X, axis=0) > 0
+            new_domain = Domain(
+                np.array(annotation_probs.domain.attributes)[col_nonzero])
+            annotation_probs = Table(new_domain, annotation_probs)
 
-        domain = Domain([ContinuousVariable(ct) for ct in annotations])
-        if len(annotations_scores) <= 0:
-            annotations_scores = np.empty((0, len(domain)))
-        scores_table = Table(domain, annotations_scores)
-        return scores_table
+        return annotation_probs
