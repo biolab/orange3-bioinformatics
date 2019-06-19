e8f61889b54560a6aa14edfde69936e36a40af5d
Primož Godec <p.godec9@gmail.com>
2019-06-19 11:03:39 +0200

    c851e6cee25ea76a4c56178db4aa7d9a02772bf7

--- .travis/install_orange.sh
+++ .travis/install_orange.sh
@@ -3,7 +3,7 @@
 if [ ${ORANGE_INSTALL} == "source" ];
 then
     echo "Orange install for source"
-    pip install --no-deps https://github.com/biolab/orange3/archive/c57f851170f01c724a7f71df4d3b35364fb451e6.zip
+    pip install --no-deps https://github.com/biolab/orange3/archive/55954d6ea2ee38b8bf4e835051b7ead29f3839b6.zip
 else
     echo "Orange install for conda"
     conda install orange3
