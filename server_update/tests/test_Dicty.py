a713abe3b799efcfbd40f50aa724a4924fcf6df8
JakaKokosar <jaka.kokosar@gmail.com>
2019-08-02 10:56:50 +0200

    server_update: remove legacy update scripts
    Serverfiles update scripts and data are now in separate repository https://github.com/JakaKokosar/bioinformatics-serverfiles

--- server_update/tests/test_Dicty.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-from orangecontrib.bioinformatics.dicty import phenotypes as DictyMutants
-
-
-class DictyMutantsTest(TestCase):
-
-    def test_size(self):
-        self.assertGreater(len(DictyMutants.gene_mutants()), 1000)
--- server_update/tests/test_GeneInfo.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-
-class GeneInfo(TestCase):
-    # TODO: test geneinfo database
-
-    def test_gene_info(self):
-        pass
