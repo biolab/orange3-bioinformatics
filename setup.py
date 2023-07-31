8bc5defc0c1d25355d0e8835757ce015cbcb6b3e
Jaka Kokošar <jaka.kokosar@gmail.com>
2023-07-31 14:22:10 +0200

    setup: update requirements

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ if __name__ == '__main__':
         use_scm_version=True,
         setup_requires=['setuptools-scm', 'setuptools>=40.0'],
         install_requires=[
-            'Orange3>=3.31.0',
-            'orange-widget-base>=4.14.1',
-            'scipy>=1.11.0',
+            'Orange3>=3.34.0',
+            'orange-widget-base>=4.19.0',
             'pyclipper>=1.2.0',
             'point-annotator~=2.0',
             'requests',
             'requests-cache>=0.8.0',
-            'serverfiles',
             'resdk>=13.8.0',
             'genesis-pyapi',
             'single_sample_gsea>=0.2.0',
             # Versions are determined by Orange
             'numpy',
+            'scipy',
+            'serverfiles',
         ],
         extras_require={
             # docutils changed html in 0.17; fixing to 0.16 until parser fixed
